824fb109ced0a03497b595d286ff8fb0ca22e422
TurtleCamera <zou.allen@yahoo.com>
2025-12-14 00:25:47 -0600

    Fixed all the syntax errors in UUIDHandler.java

--- src/client/java/com/Counter/utils/UUIDHandler.java
+++ src/client/java/com/Counter/utils/UUIDHandler.java
@@ -1,19 +1,19 @@
 package com.Counter.utils;
 
-import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.Minecraft;
 
 public class UUIDHandler {
     // Gets the UUID of the server the player is on (or single player)
     public static String getUUID() {
-        MinecraftClient client = MinecraftClient.getInstance();
+        Minecraft client = Minecraft.getInstance();
         String uuid;
-        if (client.isInSingleplayer() && client.getServer() != null) {
+        if (client.isLocalServer()) {
             // Use the same counter for all single player worlds
             uuid = "single_player";
         }
         else {
             // Use the server address (maybe consider the port)
-            uuid = client.getCurrentServerEntry().address;
+            uuid = client.getCurrentServer().ip;
             uuid = uuid.split(":")[0];
         }
 
